4db9a827f22fd26d73313715c26da32be4fd72a0
fran <fran@fran-kali.fran>
2022-03-19 20:10:14 +0100

    ED P2 20:10

---
+++ ED/P2/binary_tree/btree_utils_imp.hpp
@@ -0,0 +1,236 @@
+/**
+ * CopyRight F. J. Madrid-Cuevas <fjmadrid@uco.es>
+ *
+ * Sólo se permite el uso de este código en la docencia de las asignaturas sobre
+ * Estructuras de Datos de la Universidad de Córdoba.
+ *
+ * Está prohibido su uso para cualquier otro objetivo.
+ */
+#pragma once
+
+#include <cassert>
+#include <exception>
+#include <memory>
+#include <iostream>
+#include <queue>
+
+#include "btree_utils.hpp"
+
+template<class T>
+int compute_height (typename BTree<T>::Ref t)
+{
+    assert(t != nullptr);
+    int height = 0;
+    //TODO
+
+    //
+    return height;
+}
+
+template<class T>
+size_t compute_size (typename BTree<T>::Ref t)
+{
+    assert(t != nullptr);
+    size_t ret_val = 0;
+    //TODO
+
+    //
+    return ret_val;
+}
+
+template <class T, typename Processor>
+bool
+prefix_process(typename BTree<T>::Ref tree, Processor& p)
+{
+    assert(tree != nullptr);
+    bool retVal = true;
+    //TODO
+
+    //
+    return retVal;
+}
+
+template <class T, class Processor>
+bool
+infix_process(typename BTree<T>::Ref tree, Processor& p)
+{
+    assert(tree != nullptr);
+    bool retVal = true;
+    //TODO
+
+    //
+    return retVal;
+}
+
+template <class T, class Processor>
+bool
+postfix_process(typename BTree<T>::Ref tree, Processor& p)
+{
+    assert(tree != nullptr);
+    bool retVal = true;
+    //TODO
+
+    //
+    return retVal;
+}
+
+
+template <class T, class Processor>
+bool
+breadth_first_process(typename BTree<T>::Ref tree, Processor& p)
+{
+    assert(tree != nullptr);
+    bool go_on = true;
+    //TODO
+
+    //
+    return go_on;
+}
+
+template <class T>
+std::ostream&
+print_prefix(std::ostream& out, typename BTree<T>::Ref tree)
+{
+    //TODO
+    //You must create a lambda function with a parameter to be printed and
+    //  use a prefix_process to process the tree with this lambda.
+    //Remenber: the lambda must return true.
+
+    //
+    return out;
+}
+
+template <class T>
+std::ostream&
+print_infix(std::ostream& out, typename BTree<T>::Ref tree)
+{
+    //TODO
+    //You must create a lambda function with a parameter to be printed and
+    //  use a infix_process to process the tree with this lambda.
+    //Remenber: the lambda must return true.
+
+    //
+    return out;
+}
+
+template <class T>
+std::ostream&
+print_postfix(std::ostream& out, typename BTree<T>::Ref tree)
+{
+    //TODO
+    //You must create a lambda function with a parameter to be printed and
+    //  use a postfix_process to process the tree with this lambda.
+    //Remenber: the lambda must return true.
+
+    //
+    return out;
+}
+
+template <class T>
+std::ostream&
+print_breadth_first(std::ostream& out, typename BTree<T>::Ref tree)
+{
+    //TODO
+    //You must create a lambda function with a parameter to be printed and
+    //  use a breadth_first_process to process the tree with this lambda.
+    //Remenber: the lambda must return true.
+
+    //
+    return out;
+}
+
+template <class T>
+bool search_prefix(typename BTree<T>::Ref tree, const T& it, size_t& count)
+{
+    bool found = false;
+    count = 0;
+    //TODO
+    //You must create a lambda function with a parameter to compare it to the
+    // value of the element to be searched for.
+    // Use the lambda with in the prefix_process.
+    //Remenber: Also, the lambda must update the count variable and
+    //must return True/False.
+
+    //
+    return found;
+}
+
+template <class T>
+bool search_infix(typename BTree<T>::Ref tree, const T& it, size_t& count)
+{
+    bool found = false;
+    count = 0;
+    //TODO
+    //You must create a lambda function with a parameter to compare it to the
+    // value of the element to be searched for.
+    // Use the lambda with in the infix_process.
+    //Remenber: Also, the lambda must update the count variable and
+    //must return True/False.
+
+    //
+    return found;
+}
+
+template <class T>
+bool search_postfix(typename BTree<T>::Ref tree, const T& it, size_t& count)
+{
+    bool found = false;
+    count = 0;
+    //TODO
+    //You must create a lambda function with a parameter to compare it to the
+    // value of the element to be searched for.
+    // Use the lambda with in the postfix_process.
+    //Remenber: Also, the lambda must update the count variable and
+    //must return True/False.
+
+    //
+    return found;
+}
+
+template <class T>
+bool search_breadth_first(typename BTree<T>::Ref tree, const T& it, size_t& count)
+{
+    bool found = false;
+    count = 0;
+    //TODO
+    //You must create a lambda function with a parameter to compare it to the
+    // value of the element to be searched for.
+    // Use the lambda with in the breadth_first_process.
+    //Remenber: Also, the lambda must update the count variable and
+    //must return True/False.
+
+    //
+    return found;
+}
+
+
+template<class T>
+bool check_btree_in_order(typename BTree<T>::Ref const& tree)
+{
+    bool ret_val = true;
+    //TODO
+
+    //
+    return ret_val;
+}
+
+template<class T>
+bool has_in_order(typename BTree<T>::Ref tree, T const& v)
+{
+    assert(check_btree_in_order<T>(tree));    
+    bool ret_val = false;
+    //TODO
+
+    //
+    return ret_val;
+}
+
+template <class T>
+void insert_in_order(typename BTree<T>::Ref tree, T const& v)
+{
+    assert(check_btree_in_order<T>(tree));
+    //TODO
+
+    //
+    assert(has_in_order<T>(tree, v));
+}
